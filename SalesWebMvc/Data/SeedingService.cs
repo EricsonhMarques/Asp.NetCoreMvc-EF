aab79638c4de3781558acb590b73531ca3c789f0
Ericson Marques <ericson.henriqurjr@gmail.com>
2021-10-14 18:55:27 -0300

    SellersController

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,30 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Department.Any() ||
+                _context.Seller.Any() ||
+                _context.SalesRecord.Any())
+            {
+                return; // DB já foi populado
+            }
+
+            
+        }
+    }
+}
